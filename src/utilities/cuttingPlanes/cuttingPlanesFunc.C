06e055854e15b29d9d6a6b7c157aa44dd2ac34d6
Sam Hewitt <sam.hewitt@manchester.ac.uk>
2018-11-02 21:53:37 +0000

    Major: Function object to output plane data
    The function object cuttingPlane outputs .csv files
    that are plot as a countour map by plot_surf.py.
    
    The file yplane is an example file, that should be included
    in the system controlDict.
    
    The csv files are stored in the postProcessing dir that
    exists in the case dir for serial runs and the processor
    dirs for parallel runs.

---
+++ src/utilities/cuttingPlanes/cuttingPlanesFunc.C
@@ -0,0 +1,158 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | foam-extend: Open Source CFD
+   \\    /   O peration     | Version:     4.0
+    \\  /    A nd           | Web:         http://www.foam-extend.org
+     \\/     M anipulation  | For copyright notice see file Copyright
+-------------------------------------------------------------------------------
+License
+    This file is part of foam-extend.
+
+    foam-extend is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by the
+    Free Software Foundation, either version 3 of the License, or (at your
+    option) any later version.
+
+    foam-extend is distributed in the hope that it will be useful, but
+    WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with foam-extend.  If not, see <http://www.gnu.org/licenses/>.
+
+Author
+    Sam Hewitt, University of Manchester.  All rights reserved
+
+\*----------------------------------------------------------------------------*/
+
+#include "cuttingPlanesFunc.H"
+#include "addToRunTimeSelectionTable.H"
+#include "volFields.H"
+#include "pointFields.H"
+#include "OStringStream.H"
+#include "IStringStream.H"
+#include "IOmanip.H"
+#include "plane.H"
+#include "cuttingPlane.H"
+#include "cellSet.H"
+
+// * * * * * * * * * * * * * * Static Data Members * * * * * * * * * * * * * //
+
+namespace Foam
+{
+    defineTypeNameAndDebug(cuttingPlanesFunc, 0);
+
+    addToRunTimeSelectionTable
+    (
+        functionObject,
+        cuttingPlanesFunc,
+        dictionary
+    );
+}
+
+
+// * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
+
+bool Foam::cuttingPlanesFunc::writeData()
+{
+    const fvMesh& mesh =
+        time_.lookupObject<fvMesh>(polyMesh::defaultRegion);
+
+    plane pl1(point_,dir_);
+    cuttingPlane cutPlane(pl1,mesh);
+
+    const labelList& cutCells = cutPlane.cutCells();
+
+    if(cutCells.size()>0)
+    {
+        if (mesh.foundObject<volScalarField>("p"))
+        {
+            const volScalarField& p =
+            mesh.lookupObject<volScalarField>("p");
+
+            const volVectorField& U =
+            mesh.lookupObject<volVectorField>("U");
+         
+            fileName outFile(planeName_+"_"+time_.timeName()+".csv");
+            
+            OFstream os(time_.path()/"postProcessing"/outFile);
+            os << "# X" << tab << "Y" << tab << "Z" << tab << "p";
+            os << tab << "U(x,y,z)"<< endl;
+            forAll(cutCells,i)          
+            {
+                os << mesh.C()[cutCells[i]].x() << ",";
+                os << mesh.C()[cutCells[i]].y() << ",";
+                os << mesh.C()[cutCells[i]].z() << ",";
+                os << p[cutCells[i]] << ",";
+                os << U[cutCells[i]].x() << ",";
+                os << U[cutCells[i]].y() << ",";
+                os << U[cutCells[i]].z();
+                os << endl;
+            }
+        }        
+
+    }
+
+    return true;
+}
+
+// * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
+
+Foam::cuttingPlanesFunc::cuttingPlanesFunc
+(
+    const word& name,
+    const Time& t,
+    const dictionary& dict
+)
+:
+    functionObject(name),
+    name_(name),
+    time_(t),
+    point_(dict.lookup("point")),
+    dir_(dict.lookup("dir")),
+    planeName_(dict.lookup("name"))
+{
+    Info << "Creating " << this->name() << " function object." << endl;
+
+    // Create Pressure dir if not already created
+    fileName outputDir;
+    if (Pstream::parRun())
+    {
+        // Put in undecomposed case (Note: gives problems for
+        // distributed data running)
+        outputDir = time_.path()/"postProcessing";
+    }
+    else
+    {
+        outputDir = time_.path()/"postProcessing";
+    }
+
+    if(!isDir(outputDir))
+    {
+        // Create directory if does not exist.
+        mkDir(outputDir);
+    }
+}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+bool Foam::cuttingPlanesFunc::start()
+{
+    return writeData();
+}
+
+
+bool Foam::cuttingPlanesFunc::execute()
+{
+    return writeData();
+}
+
+
+bool Foam::cuttingPlanesFunc::read(const dictionary& dict)
+{
+    return true;
+}
+
+// ************************************************************************* //
---
+++ src/utilities/plot_surf.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python2.7
+
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from matplotlib import rc
+
+import numpy as np
+
+from scipy.interpolate import griddata
+
+
+def read_csv(fname):
+    '''
+    Routine reads a csv file with ',' delimiter
+    :param fname: The filename to be read
+    :return: data: np.array of values
+    '''
+    data = np.loadtxt(fname,delimiter=',')
+    return data
+
+def check_parallel():
+    '''
+    Very crude method of checking if case was run in serial
+    or Parallel. It looks at the dir list and searches for a
+    dir with the prefix processor
+    :return: flag: bool
+    '''
+    flag=False
+
+    dir_list = os.listdir(os.getcwd())
+    for i in dir_list:
+        if 'processor' in i:
+            flag = True
+            return flag
+
+    return flag
+
+def get_num_proc():
+    '''
+    Crude way of counting processors
+    :return: npes: number of processors
+    '''
+    npes = 0
+
+    dir_list = os.listdir(os.getcwd())
+    for i in dir_list:
+        if 'processor' in i:
+            npes=npes+1
+
+    return npes
+
+if __name__ == '__main__':
+
+    # Heading
+    print("\n-------------------------------------------")
+    print("Plotting Surface ")
+    print("-------------------------------------------\n")
+
+    ################ USER PARAMETERS #####################
+    value = 'p' # Pressure
+    time  = '0.01'
+    #value = 'U' # Velocity
+    ######################################################
+
+    # Check if parallel
+    parallel = check_parallel()
+
+    # Get Number of processors
+    if (parallel):
+        npes = get_num_proc()
+        print("Number of Processors: {0}".format(npes))
+
+    # Extract Data for given timestep
+    if(parallel):
+        x_c = []
+        y_c = []
+        z_c = []
+        for i in range(npes):
+            fname='processor'+str(i)+'/postProcessing/zplane_0.01.csv'
+            if os.path.isfile(fname):
+                data = read_csv(fname)
+                x_c.extend(data[:, 0])
+                y_c.extend(data[:, 1])
+                z_c.extend(data[:, 4])
+    else:
+        fname='postProcessing/zplane_0.01.csv'
+        if os.path.isfile(fname):
+            data = read_csv(fname)
+            x_c = data[:,0]
+            y_c = data[:,1]
+            z_c = data[:,4]
+
+    x_c=np.ravel(np.array(x_c))
+    y_c=np.ravel(np.array(y_c))
+    z_c=np.ravel(np.array(z_c))
+
+    # Set fonts
+    rc('font', **{'family': 'serif', 'serif': ['Computer Modern'], 'size': 12})
+    rc('text', usetex=True)
+
+    # Create figure and axes
+    fig,ax = plt.subplots(figsize=(5.51,3.5))
+
+    # Plot the points
+    #plt.scatter(x_c, y_c, marker='o', s=5, zorder=10)
+
+    # Plot the basic data, similar to cell plots in paraview
+    #zic = griddata((x_c, y_c), z_c, (x_c[None, :], y_c[:, None]))
+    #plt.pcolormesh(x_c, y_c, zic, cmap=plt.get_cmap('rainbow'))
+
+    # Plot interpolated data
+    # Create grid for interpolations
+    # Last input parameter is the number of grid points (10000) is large
+    xic = np.linspace(min(x_c), max(x_c), 1000)
+    yic = np.linspace(min(y_c), max(y_c), 1000)
+
+    zic = griddata((x_c, y_c), z_c, (xic[None,:],yic[:,None]))
+    plt.contourf(xic,yic,zic,12,cmap=plt.get_cmap('RdGy'),linewidths = 0.5)
+    cbar = plt.colorbar()
+    cbar.set_label("Pressure")
+    plt.contour(xic, yic, zic, 12,linewidths=0.5,colors='k')
+
+    #plt.contour(xic,yic,zic,12,linewidths = 0.5, colors = 'k')
+    #plt.pcolormesh(xic, yic, zic,cmap = plt.get_cmap('spectral'))
+
+
+
+    # Create a Rectangle patch
+    # Add the patch to the Axes
+    #rect = patches.Rectangle((-0.5,-0.5),1,1,linewidth=1,edgecolor='k',facecolor='k',alpha=1.0)
+    rect = patches.Rectangle((-0.5, 0), 1, 4, linewidth=1, edgecolor='k', facecolor='k', alpha=1.0)
+    ax.add_patch(rect)
+
+
+    plt.show()
